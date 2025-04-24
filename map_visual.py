2754dac0eb22fd47effec3c9e7461ba0ba76287f
BarnabyRumbold <94046623+BarnabyRumbold@users.noreply.github.com>
2025-04-24 15:26:37 +0100

    Update map_visual.py

--- map_visual.py
+++ map_visual.py
@@ -10,11 +10,11 @@ import numpy as np
 st.set_page_config(layout="wide")
 
 # Add Mammal Society logo to side bar
-st.sidebar.markdown("""
-    <div style="text-align: center;">
-        <img src="https://images.squarespace-cdn.com/content/v1/654a3265fcbd755384b0552f/5cc2d0e2-dd55-449f-ae3a-949ab6871318/MSlogo_colour_strapblue_L-800px.jpeg?format=1500w" alt="Logo" width="100"/>
-    </div>
-""", unsafe_allow_html=True)
+# st.sidebar.markdown("""
+#     <div style="text-align: center;">
+#         <img src="https://images.squarespace-cdn.com/content/v1/654a3265fcbd755384b0552f/5cc2d0e2-dd55-449f-ae3a-949ab6871318/MSlogo_colour_strapblue_L-800px.jpeg?format=1500w" alt="Logo" width="100"/>
+#     </div>
+# """, unsafe_allow_html=True)
 
 # Add header and page description
 st.write("### Otter Sightings Over Time (Map Visual)")
